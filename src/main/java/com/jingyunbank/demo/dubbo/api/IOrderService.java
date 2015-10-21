31ed9343ae780a395a87e569c19d97636e10dc3b
Mike Cao <michael-cao@outlook.com>
2015-10-21 09:26:53 +0800

    init commit.

---
+++ src/main/java/com/jingyunbank/demo/dubbo/api/IOrderService.java
@@ -0,0 +1,8 @@
+package com.jingyunbank.demo.dubbo.api;
+
+public interface IOrderService {
+
+	public void order(Order order);
+	
+}
+
---
+++ src/main/java/com/jingyunbank/demo/dubbo/api/Order.java
@@ -0,0 +1,9 @@
+package com.jingyunbank.demo.dubbo.api;
+
+import java.io.Serializable;
+
+public class Order implements Serializable{
+
+	private static final long serialVersionUID = -7797906297897206435L;
+
+}
